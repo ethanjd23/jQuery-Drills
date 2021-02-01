688330bd2c9d8926779feffc6f610a27ba0938bb
ethanjd23 <ethanjd23@gmail.com>
2021-02-01 15:00:17 -0600

    first commit, few steps in

---
+++ app.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+    $("<input type='submit' id='btnSubmit' value='Submit'></input>").appendTo("body")
+    $('#btnSubmit').click(function() {
+        alert("Nother hing.")
+    })
+})
---
+++ readme.md
@@ -0,0 +1,4 @@
+Simple jQuery Drills
+Objective
+
+Practice using the fundamentals of jQuery that were explored in lecture.
