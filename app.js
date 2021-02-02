1612c3d9fab9817de9c288d3650929beea8b1501
ethanjd23 <ethanjd23@gmail.com>
2021-02-01 21:41:53 -0600

    finished

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ let h2Count = 1
 $(document).ready(function() {
     $("<input type='button' id='btnSubmit' value='Submit'></input>").appendTo("form")
     $("body").append("<div class='div'></div>")
+    $("body").append("<ul class='ul'></ul>");
 
     $("#btnSubmit").attr("disabled", true);
     $("#text-box").keyup (function() {
@@ -15,25 +16,39 @@ $(document).ready(function() {
     })
 
     $('#btnSubmit').click(function() {
-        let msg = $("#text-box").val();
-        $(".div").append("<h2 class = 'h2' id = " + h2Count + ">" + msg + "</h2>");
-        $("#" + h2Count).hover(h2Hover);
-        h2Count++;
+        let msg = $("#text-box").val(); 
+        // $(".div").append("<h2 class = 'h2' id = " + h2Count + ">" + msg + "</h2>"); // creating and inserting text box entry in to an h2 inside of div with incrementing id 
+        // $("#" + h2Count).hover(h2Hover, h2NonHover); // changing bg color and border radius on hover for h2 selected
+        // h2Count++;
+
+        $(".ul").append("<li class='li'>" + msg + "</li>");
+        $(".li").click(liStyler);
+        $(".li").dblclick(liRemover);
     })
 
-    
 })
 
 function h2Hover() {
-    $(":target").css({
+    $(event.target).css({
         "background-color": "navy",
         "border-radius": "25%"
     })
-}
+};
 
 function h2NonHover() {
-    $(":target").css({
+    $(event.target).css({
         "background-color": "transparent",
         "border-radius": "0%"
     })
+};
+
+function liStyler() {
+    let genColor = Math.floor(Math.random()*16777215).toString(16); 
+    $(event.target).css({
+        "color": "#" + genColor,
+    })
+};
+
+function liRemover() {
+    $(event.target).remove();
 }
